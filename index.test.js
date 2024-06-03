cb17faad22849b66dc5f983390c1ed7a6bc5b97c
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-06-03 11:16:12 +0000

    Added source code

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const { system24 } = require('./index');
+
+test('system24 returns results from the 15/5 poll', () => {
+    expect(system24([32.5, 16, 13, 9, 10, 2.2, 3.3, 3.5, 2.4, 2.9, 2.5], 3)).toStrictEqual([9, 4, 3, 2, 2, 0, 0, 1, 0, 0, 0]);
+});
+
+test('system24 returns results from the 13/5 poll', () => {
+    expect(system24([33.3, 17.2, 12.2, 6.7, 7.8, 2.7, 3.4, 5.0], 3)).toStrictEqual([9, 5, 3, 1, 2, 0, 0, 1]);
+});
+
+test('system24 returns results from the 25/4 poll', () => {
+    expect(system24([33.4, 14.7, 11.4, 8.5, 9.8, 2.4, 5.4, 2.4, 3.4, 2.2, 2.5, 1.7], 3)).toStrictEqual([9, 4, 3, 2, 2, 0, 1, 0, 0, 0, 0, 0]);
+});
+
+test('system24 returns results from the 22/4 poll', () => {
+    expect(system24([33.6, 16.3, 13.5, 8.8, 9.1, 2.1, 3.1, 2.4, 3.4, 2.7, 2.2], 3)).toStrictEqual([9, 4, 3, 2, 2, 0, 0, 0, 1, 0, 0]);
+});
