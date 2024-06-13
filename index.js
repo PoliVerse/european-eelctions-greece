904127e755a5ca7eb2f4b4f45833b67d9351a070
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-06-13 13:51:40 +0000

    Updated function to use 2 seat distributions

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const system24 = (percentages, threshold) => {
             let seats = 21;
             let seatDistribution = new Array(percentages.length).fill(0);
             let eligiblePercentages = percentages.map((percentage, index) => ({ percentage, index })).filter(p => p.percentage >= threshold);        
+            let totalPercentage = eligiblePercentages.reduce((a, b) => a + b.percentage, 0).toFixed(2);
+            let firstDistribution = (totalPercentage / seats).toFixed(2);
+            let remainingSeats = 21; 
+            let unusedPercentages = new Array(eligiblePercentages.length).fill(0);
 
-            for (let i = 0; i < seats; i++) {
-                let maxQuotient = 0;
-                let maxIndex = -1;
-                let remainingSeats = seats - i;
+            for (let i = 0; i < eligiblePercentages.length; i++) {
+                let quotient = Math.floor(eligiblePercentages[i].percentage / firstDistribution);
+                seatDistribution[eligiblePercentages[i].index] = quotient;
+                remainingSeats -= quotient;
+                unusedPercentages[i] = (eligiblePercentages[i].percentage - (firstDistribution * quotient)).toFixed(2);
+            }
 
-                eligiblePercentages.forEach(party => {
-                    let quotient = (party.percentage / (seatDistribution[party.index] + 1));
-                    if (quotient > maxQuotient) { 
-                        maxQuotient = quotient;
-                        maxIndex = party.index;
-                    }
-                });
-
-                seatDistribution[maxIndex]++;
+            while (remainingSeats > 0) {
+                let maxUnusedIndex = unusedPercentages.reduce((maxIndex, currentValue, currentIndex, array) => currentValue > array[maxIndex] ? currentIndex : maxIndex, 0);
+                seatDistribution[eligiblePercentages[maxUnusedIndex].index]++;
+                unusedPercentages[maxUnusedIndex] = 0;
                 remainingSeats--;
             }
 
